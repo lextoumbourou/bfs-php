498f26196ce4258bab79895ee9950ec26a5c8f19
Lex Toumbourou <lextoumbourou@gmail.com>
2014-01-14 17:22:31 +1100

    Whitespace cleanup

--- tests/unit.php
+++ tests/unit.php
@@ -12,8 +12,8 @@ class BFSTest extends PHPUnit_Framework_TestCase
             'D' => ['B'],
             'C' => ['A',],
         ];
-        $this->assertTrue(bfs($Graph, 'A', 'D'));
 
+        $this->assertTrue(bfs($Graph, 'A', 'D'));
         $this->assertFalse(bfs($Graph, 'A', 'F'));
     }
 
@@ -26,9 +26,9 @@ class BFSTest extends PHPUnit_Framework_TestCase
             'C' => ['A', 'E'],
             'E' => ['C', 'B', 'D'],
         ];
+
         $this->assertEquals(bfs_path($Graph, 'A', 'D'), ['A', 'B', 'D']);
         $this->assertEquals(bfs_path($Graph, 'A', 'E'), ['A', 'C', 'E']);
-
         $this->assertFalse(bfs_path($Graph, 'A', 'F'));
     }
 }
